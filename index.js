48eca0b2d0b5edc7a0d3b406b992819c8f541c6d
pratik.c <pratik.c@FWS-LP-1019>
2023-04-06 21:38:52 +0530

    Fixed the login api.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express()
 const port = 3000
-const User=require('./models/userModel');
-const Transaction=require('./models/transactionModel');
+const User = require('./models/userModel');
+const Transaction = require('./models/transactionModel');
 
 app.use(express.json());
 app.use(cors());
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to SF06 API!')
 })
 
-app.get('/users', async(req, res) => {
+app.get('/users', async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).json(users);
@@ -24,10 +24,10 @@ app.get('/users', async(req, res) => {
   }
 })
 
-app.get('/users/:id', async(req, res) => {
+app.get('/users/:id', async (req, res) => {
   try {
-    const {id}=req.params;
-    const user = await User.find({_id:id});
+    const { id } = req.params;
+    const user = await User.find({ _id: id });
     res.status(200).json(user);
   }
   catch (error) {
@@ -36,11 +36,14 @@ app.get('/users/:id', async(req, res) => {
   }
 })
 
-app.get('/users/login/:passcode', async(req, res) => {
+app.get('/users/login/:passcode', async (req, res) => {
   try {
-    const {passcode}=req.params;
-    const user = await User.find({passCode:passcode});
-    res.status(200).json({"isValid":true,"data":user});
+    const { passcode } = req.params;
+    const user = await User.findOne({ passCode: passcode });
+    if (user == [])
+      res.status(200).json({ "isValid": false, "data": null });
+    else
+      res.status(404).json({ "isValid": true, "data": user });
   }
   catch (error) {
     console.log(error.message);
@@ -48,7 +51,7 @@ app.get('/users/login/:passcode', async(req, res) => {
   }
 })
 
-app.post('/users', async(req, res) => {
+app.post('/users', async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(200).json(user);
@@ -59,11 +62,11 @@ app.post('/users', async(req, res) => {
   }
 })
 
-app.put('/users/:id', async(req, res) => {
+app.put('/users/:id', async (req, res) => {
   try {
-    const {id}=req.params;
-    const user = await User.findByIdAndUpdate(id,req.body);
-    const updatedUser=await User.findById(id);
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, req.body);
+    const updatedUser = await User.findById(id);
     res.status(200).json(updatedUser);
   }
   catch (error) {
@@ -72,11 +75,11 @@ app.put('/users/:id', async(req, res) => {
   }
 })
 
-app.delete('/users/:id', async(req, res) => {
+app.delete('/users/:id', async (req, res) => {
   try {
-    const {id}=req.params;
+    const { id } = req.params;
     const user = await User.findByIdAndDelete(id);
-    res.status(200).json({message:"User has been deleted!"});
+    res.status(200).json({ message: "User has been deleted!" });
   }
   catch (error) {
     console.log(error.message);
@@ -84,7 +87,7 @@ app.delete('/users/:id', async(req, res) => {
   }
 })
 
-app.post('/transactions', async(req, res) => {
+app.post('/transactions', async (req, res) => {
   try {
     const transaction = await Transaction.create(req.body);
     res.status(200).json(transaction);
@@ -95,7 +98,7 @@ app.post('/transactions', async(req, res) => {
   }
 })
 
-app.get('/transactions', async(req, res) => {
+app.get('/transactions', async (req, res) => {
   try {
     const transactions = await Transaction.find();
     res.status(200).json(transactions);
@@ -106,10 +109,10 @@ app.get('/transactions', async(req, res) => {
   }
 })
 
-app.get('/transactions/:id', async(req, res) => {
+app.get('/transactions/:id', async (req, res) => {
   try {
-    const {id}=req.params;
-    const transaction = await Transaction.find({_id:id});
+    const { id } = req.params;
+    const transaction = await Transaction.find({ _id: id });
     res.status(200).json(transaction);
   }
   catch (error) {
@@ -118,11 +121,11 @@ app.get('/transactions/:id', async(req, res) => {
   }
 })
 
-app.put('/transactions/:id', async(req, res) => {
+app.put('/transactions/:id', async (req, res) => {
   try {
-    const {id}=req.params;
-    const transaction = await Transaction.findByIdAndUpdate(id,req.body);
-    const updatedTransactions=await Transaction.findById(id);
+    const { id } = req.params;
+    const transaction = await Transaction.findByIdAndUpdate(id, req.body);
+    const updatedTransactions = await Transaction.findById(id);
     res.status(200).json(updatedTransactions);
   }
   catch (error) {
@@ -131,11 +134,11 @@ app.put('/transactions/:id', async(req, res) => {
   }
 })
 
-app.delete('/transactions/:id', async(req, res) => {
+app.delete('/transactions/:id', async (req, res) => {
   try {
-    const {id}=req.params;
+    const { id } = req.params;
     const transaction = await Transaction.findByIdAndDelete(id);
-    res.status(200).json({message:"Transaction has been deleted!"});
+    res.status(200).json({ message: "Transaction has been deleted!" });
   }
   catch (error) {
     console.log(error.message);
@@ -143,11 +146,11 @@ app.delete('/transactions/:id', async(req, res) => {
   }
 })
 
-app.get('/transactions/byDate/:date', async(req, res) => {
+app.get('/transactions/byDate/:date', async (req, res) => {
   try {
-    const {date}=req.params;
+    const { date } = req.params;
     const transactions = await Transaction.find();
-    const transactionsByDate=transactions.filter(t=>t.timeStamp.toISOString().split('T')[0]==date);
+    const transactionsByDate = transactions.filter(t => t.timeStamp.toISOString().split('T')[0] == date);
     res.status(200).json(transactionsByDate);
   }
   catch (error) {
@@ -156,12 +159,12 @@ app.get('/transactions/byDate/:date', async(req, res) => {
   }
 })
 
-app.get('/transactions/byWeek/:startDay/:endDay', async(req, res) => {
+app.get('/transactions/byWeek/:startDay/:endDay', async (req, res) => {
   try {
-    const {startDay}=req.params;
-    const {endDay}=req.params;
-    const actualDay=new Date(endDay+"T23:59:59.000Z");
-    const transactions = await Transaction. find({ timeStamp: { $gte: startDay, $lte: actualDay } });
+    const { startDay } = req.params;
+    const { endDay } = req.params;
+    const actualDay = new Date(endDay + "T23:59:59.000Z");
+    const transactions = await Transaction.find({ timeStamp: { $gte: startDay, $lte: actualDay } });
     res.status(200).json(transactions);
   }
   catch (error) {
